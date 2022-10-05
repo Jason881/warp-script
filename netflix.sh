fafdd6cd43eaf369f67325b9596db773a2b7c1b8
MisakaNo の 小破站 <misakablog@protonmail.com>
2022-10-05 00:26:53 +0000

    Update netflix.sh

--- netflix.sh
+++ netflix.sh
@@ -148,6 +148,99 @@ wgcfnfd(){
     fi
 }
 
+wpgonf4(){
+    wgcfv4=$(curl -s4m8 https://www.cloudflare.com/cdn-cgi/trace -k | grep warp | cut -d= -f2)
+    if [[ ! $wgcfv4 =~ on|plus ]]; then
+        red "WARP-Go的IPv4未正常配置，请在脚本中安装WARP-Go全局模式！"
+        exit 1
+    fi
+    nfv4result=$(nf | sed -n 3p | sed -r "s/\x1B\[([0-9]{1,2}(;[0-9]{1,2})?)?[m|K]//g")
+    if [[ $nfv4result == "您的出口IP完整解锁Netflix，支持非自制剧的观看" ]]; then
+        WgcfWARPIP=$(curl -s4m8 api64.ipify.org -k)
+        green "当前WARP-Go的IP：$WgcfWARPIP 已解锁Netfilx"
+        yellow "等待1小时后，脚本将会自动重新检查Netfilx解锁状态"
+        sleep 1h
+        wgcfnf4
+    fi
+    if [[ $nfv4result =~ "Netflix在您的出口IP所在的国家不提供服务"|"Netflix在您的出口IP所在的国家提供服务，但是您的IP疑似代理，无法正常使用服务"|"您的出口IP可以使用Netflix，但仅可看Netflix自制剧" ]]; then
+        WgcfWARPIP=$(curl -s4m8 api64.ipify.org -k)
+        red "当前WARP-Go的IP：$WgcfWARPIP 未解锁Netfilx，脚本将在15秒后重新测试Netfilx解锁情况"
+        sleep 15
+        systemctl stop warp-go
+        systemctl start warp-go
+        wgcfnf4
+    fi
+    if [[ $nfv4result == "您的网络可能没有正常配置IPv4，或者没有IPv4网络接入" ]]; then
+        systemctl stop warp-go
+        systemctl start warp-go
+        wgcfnf4
+    fi
+}
+
+wpgonf6(){
+    wgcfv6=$(curl -s6m8 https://www.cloudflare.com/cdn-cgi/trace -k | grep warp | cut -d= -f2)
+    if [[ ! $wgcfv6 =~ on|plus ]]; then
+        red "WARP-Go的IPv6未正常配置，请在脚本中安装WARP-Go全局模式！"
+        exit 1
+    fi
+    nfv6result=$(nf | sed -n 7p | sed -r "s/\x1B\[([0-9]{1,2}(;[0-9]{1,2})?)?[m|K]//g")
+    if [[ $nfv6result == "您的出口IP完整解锁Netflix，支持非自制剧的观看" ]]; then
+        WgcfWARPIP=$(curl -s6m8 api64.ipify.org -k)
+        green "当前WARP-Go的IP：$WgcfWARPIP 已解锁Netfilx"
+        yellow "等待1小时后，脚本将会自动重新检查Netfilx解锁状态"
+        sleep 1h
+        wgcfnf6
+    fi
+    if [[ $nfv6result =~ "Netflix在您的出口IP所在的国家不提供服务"|"Netflix在您的出口IP所在的国家提供服务，但是您的IP疑似代理，无法正常使用服务"|"您的出口IP可以使用Netflix，但仅可看Netflix自制剧" ]]; then
+        WgcfWARPIP=$(curl -s6m8 api64.ipify.org -k)
+        red "当前WARP-Go的IP：$WgcfWARPIP 未解锁Netfilx，脚本将在15秒后重新测试Netfilx解锁情况"
+        sleep 15
+        systemctl stop warp-go
+        systemctl start warp-go
+        wgcfnf6
+    fi
+    if [[ $nfv6result == "您的网络可能没有正常配置IPv6，或者没有IPv6网络接入" ]]; then
+        systemctl stop warp-go
+        systemctl start warp-go
+        wgcfnf6
+    fi
+}
+
+wpgonfd(){
+    wgcfv4=$(curl -s4m8 https://www.cloudflare.com/cdn-cgi/trace -k | grep warp | cut -d= -f2)
+    wgcfv6=$(curl -s6m8 https://www.cloudflare.com/cdn-cgi/trace -k | grep warp | cut -d= -f2)
+    if [[ ! $wgcfv4 =~ on|plus || ! $wgcfv6 =~ on|plus ]]; then
+        red "WARP-Go的IPv4和IPv6未正常配置，请在脚本中安装WARP-Go全局模式！"
+        exit 1
+    fi
+    nfv4result=$(nf | sed -n 3p)
+    nfv6result=$(nf | sed -n 7p)
+    if [[ $nfv4result == "您的出口IP完整解锁Netflix，支持非自制剧的观看" ]] && [[ $nfv6result == "您的出口IP完整解锁Netflix，支持非自制剧的观看" ]]; then
+        WgcfWARPV4IP=$(curl -s4m8 api64.ipify.org -k)
+        WgcfWARPV6IP=$(curl -s6m8 api64.ipify.org -k)
+        green "当前WARP-Go的IPv4 IP：$WgcfWARPV4IP 已解锁Netfilx"
+        green "当前WARP-Go的IPv6 IP：$WgcfWARPV6IP 已解锁Netfilx"
+        yellow "等待1小时后，脚本将会自动重新检查Netfilx解锁状态"
+        sleep 1h
+        wgcfnfd
+    fi
+    if [[ $nfv4result =~ "Netflix在您的出口IP所在的国家不提供服务"|"Netflix在您的出口IP所在的国家提供服务，但是您的IP疑似代理，无法正常使用服务"|"您的出口IP可以使用Netflix，但仅可看Netflix自制剧" ]] && [[ $nfv6result =~ "Netflix在您的出口IP所在的国家不提供服务"|"Netflix在您的出口IP所在的国家提供服务，但是您的IP疑似代理，无法正常使用服务"|"您的出口IP可以使用Netflix，但仅可看Netflix自制剧" ]]; then
+        WgcfWARPV4IP=$(curl -s4m8 api64.ipify.org -k)
+        WgcfWARPV6IP=$(curl -s6m8 api64.ipify.org -k)
+        red "当前WARP-Go的IPv4 IP：$WgcfWARPIP 未解锁Netfilx，脚本将在15秒后重新测试Netfilx解锁情况"
+        red "当前WARP-Go的IPv6 IP：$WgcfWARPIP 未解锁Netfilx，脚本将在15秒后重新测试Netfilx解锁情况"
+        sleep 15
+        systemctl stop warp-go
+        systemctl start warp-go
+        wgcfnfd
+    fi
+    if [[ $nfv4result == "您的网络可能没有正常配置IPv4，或者没有IPv4网络接入" ]] && [[ $nfv6result == "您的网络可能没有正常配置IPv6，或者没有IPv6网络接入" ]]; then
+        systemctl stop warp-go
+        systemctl start warp-go
+        wgcfnfd
+    fi
+}
+
 cliquan(){
     warpstat=$(curl -s4m8 https://www.cloudflare.com/cdn-cgi/trace -k --interface CloudflareWARP | grep warp | cut -d= -f2)
     if [[ ! $warpstat =~ on|plus ]]; then
@@ -243,9 +336,12 @@ menu(){
     echo -e " ${GREEN}1.${PLAIN} Wgcf-WARP 全局单栈模式 ${YELLOW}(WARP IPv4)${PLAIN}"
     echo -e " ${GREEN}2.${PLAIN} Wgcf-WARP 全局单栈模式 ${YELLOW}(WARP IPv6)${PLAIN}"
     echo -e " ${GREEN}3.${PLAIN} Wgcf-WARP 全局双栈模式 ${YELLOW}(WARP IPv4 + WARP IPv6)${PLAIN}"
-    echo -e " ${GREEN}4.${PLAIN} WARP-Cli 全局模式 ${YELLOW}(WARP IPv4)${PLAIN}"
-    echo -e " ${GREEN}5.${PLAIN} WARP-Cli 代理模式"
-    echo -e " ${GREEN}6.${PLAIN} WireProxy-WARP 代理模式"
+    echo -e " ${GREEN}4.${PLAIN} WARP-Go 全局单栈模式 ${YELLOW}(WARP IPv4)${PLAIN}"
+    echo -e " ${GREEN}5.${PLAIN} WARP-Go 全局单栈模式 ${YELLOW}(WARP IPv6)${PLAIN}"
+    echo -e " ${GREEN}6.${PLAIN} WARP-Go 全局双栈模式 ${YELLOW}(WARP IPv4 + WARP IPv6)${PLAIN}"
+    echo -e " ${GREEN}7.${PLAIN} WARP-Cli 全局模式 ${YELLOW}(WARP IPv4)${PLAIN}"
+    echo -e " ${GREEN}8.${PLAIN} WARP-Cli 代理模式"
+    echo -e " ${GREEN}9.${PLAIN} WireProxy-WARP 代理模式"
     echo " -------------"
     echo -e " ${GREEN}0.${PLAIN} 退出脚本"
     echo ""
@@ -254,9 +350,12 @@ menu(){
         1 ) wgcfnf4 ;;
         2 ) wgcfnf6 ;;
         3 ) wgcfnfd ;;
-        4 ) cliquan ;;
-        5 ) clisocks ;;
-        6 ) wireproxy ;;
+        4 ) wpgonf4 ;;
+        5 ) wpgonf6 ;;
+        6 ) wpgonfd ;;
+        7 ) cliquan ;;
+        8 ) clisocks ;;
+        9 ) wireproxy ;;
         * ) exit 1 ;;
     esac
 }
