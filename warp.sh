b9b21974f221243e0e85580651342690ac321885
MisakaNo の 小破站 <misakablog@protonmail.com>
2022-10-10 18:26:52 +0000

    Update warp.sh

--- warp.sh
+++ warp.sh
@@ -90,7 +90,7 @@ archAffix(){
 }
 
 if [[ ! -f /usr/local/bin/nf ]]; then
-    wget https://cdn.jsdelivr.net/gh/taffychan/warp/netflix/verify/nf_linux_$(archAffix) -O /usr/local/bin/nf
+    wget https://gitlab.com/misakablog/warp-script/-/raw/main/files/nfverify/nf_linux_$(archAffix) -O /usr/local/bin/nf
     chmod +x /usr/local/bin/nf
 fi
 
